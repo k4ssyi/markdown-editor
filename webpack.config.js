73cbc1e7049f5f912c45d8178dd68476d600d69e
k4ssyi <kasikasi00@gmail.com>
2022-10-20 01:03:12 +0900

    installed TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
